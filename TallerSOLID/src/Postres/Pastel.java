ceda3de1769d3feac56439cc2b1a4fa38efa1ebb
Lizz25 <73801237+Lizz25@users.noreply.github.com>
2021-06-10 15:10:02 -0500

    Cambios literales 1, 2 y 3

--- TallerSOLID/src/Postres/Pastel.java
+++ TallerSOLID/src/Postres/Pastel.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Postres;
 
 import java.util.ArrayList;
@@ -12,33 +7,18 @@ import Adicionales.Aderezo;
  *
  * @author Pedro Mendoza
  */
-public class Pastel{
-    private String sabor;
-    private double precioParcial;
-    private ArrayList<Aderezo> aderezos;
-    
-    public Pastel(String sabor){
-        aderezos= new ArrayList<>();
-        this.sabor=sabor;
-        this.precioParcial = 15.55;
-    }
-    
-    public double calcularPrecioFinal(){
-        double precioFinal;
-        precioFinal=(precioParcial+(precioParcial*0.12))+(aderezos.size()*0.50);
-        return precioFinal;
-    }
 
-    public ArrayList<Aderezo> getAderezos() {
-        return aderezos;
-    }
-
-    @Override
-    public String toString() {
-        return "Pastel{" + "sabor=" + sabor + ", precioParcial=" + precioParcial + ", aderezos=" + aderezos + '}';
-    }
-    public String showPrecioFinal(){
-        return "Precio Final: $ " + calcularPrecioFinal();
-    }
-    
+public class Pastel extends Postre {
+	
+	public Pastel(String sabor) {
+		super.precioParcial = 15.55;
+		super.sabor = sabor;
+		super.aderezos = new ArrayList<>();
+	}
+	
+	public String toString() {
+		return "Pastel{" + "sabor=" + sabor + ", precioParcial=" + precioParcial + ", aderezos=" + aderezos + '}';
+	}
+	
 }
+
